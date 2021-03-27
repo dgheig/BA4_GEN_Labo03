b3f4593afd34e716d6be8415f11872eed2a639ec
YanickHEIG <yanick.thomann@heig-vd.ch>
2021-03-27 15:18:00 +0100

    Bugged version for coop debugging

--- Threads/Banque/src/main/java/ch/heig/dgyt/banque/Transferts.java
+++ Threads/Banque/src/main/java/ch/heig/dgyt/banque/Transferts.java
@@ -1,13 +1,15 @@
 package ch.heig.dgyt.banque;
 import java.util.Objects;
+import java.util.Random;
 
 public class Transferts extends Thread {
 
     private Banque banque;
     private int nbTransfertToDo;
+    private Random rand = new Random(System.currentTimeMillis());
 
     Transferts(int nbTransfertToDo, Banque banque) {
-        Objects.requireNonNull(banque);
+        Objects.requireNonNull(banque, "TRANSFERTS CONSTRUCTOR: Bank cannot be null");
         if(nbTransfertToDo < 1)
             throw new RuntimeException("Transfert number must be strictly positive");
         this.banque = banque;
@@ -22,8 +24,24 @@ public class Transferts extends Thread {
         this(nbTransfertToDo, 5);
     }
 
+
+
     @Override
     public void run() {
-        super.run();
+        //super.run();
+        for (int i = 0; i < nbTransfertToDo; i++){
+            int fromAccount = rand.nextInt(banque.getNbComptes());
+            int toAccount = rand.nextInt(banque.getNbComptes());
+            int amount = rand.nextInt(10);
+            banque.transfert(fromAccount, toAccount, amount);
+            //System.out.println("Transferring money: " + fromAccount + "->" + toAccount + ": " + amount ". Recipient now has: " + Bank.);
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
+
 }
--- Threads/Banque/src/test/java/ch/heig/dgyt/banque/TransfertsTests.java
+++ Threads/Banque/src/test/java/ch/heig/dgyt/banque/TransfertsTests.java
@@ -1,10 +1,10 @@
 package ch.heig.dgyt.banque;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.Assert.*;
+
 
 public class TransfertsTests {
 
@@ -12,11 +12,18 @@ public class TransfertsTests {
 
     @BeforeEach
     public void setUp() throws Exception {
-        banque = new Banque(1000);
+        banque = new Banque(10);
+        System.out.println("Bank is null: " + banque==null);
     }
 
     @Test
     public void checkConsistency() throws InterruptedException {
-
+        //Banque bank = new Banque(10);
+        for (int i = 0; i < 1000; i++){
+            Transferts trans = new Transferts(1000, banque);
+            trans.start();
+        }
+        assertTrue(banque.consistent());
     }
+
 }
